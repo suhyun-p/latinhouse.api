7558db03a7cc5718cb99ac9ea9fecf06281a6888
suhpark <laztwo@hanmail.net>
2021-10-28 22:16:43 +0900

    수정

--- build/resources/main/Data.sql
+++ build/resources/main/Data.sql
@@ -2,19 +2,8 @@ insert into member_main (nickname, sex, is_instructor) values ('브라우니', '
 insert into member_main (nickname, sex, is_instructor) values ('칼리', 'F', true);
 insert into member_main (nickname, sex, is_instructor) values ('스칼렛', 'F', true);
 
-insert into class_main (title,
-                        status,
-                        genre,
-                        region,
-                        instructor_no_1,
-                        instructor_no_2,
-                        location,
-                        price)
-values ('소수집중티칭 - 스타일링 안무반',
-        'S2',
-        'B',
-        'HD',
-        3,
-        1,
-        '진심 스튜디오',
-        100000);
+insert into class_main (title, status, genre, region, instructor_no_1, instructor_no_2, location, price)
+values ('소수집중티칭 - 스타일링 안무반', 'S2', 'B', 'HD', 3, 1, '진심 스튜디오', 100000);
+
+insert into class_main (title, status, genre, region, instructor_no_1, instructor_no_2, location, price)
+values ('소수집중티칭 - 스타일링 안무반', 'S2', 'B', 'HD', 3, null, '진심 스튜디오', 100000);
